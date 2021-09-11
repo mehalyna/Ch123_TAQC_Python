c9378685d49a57b7327c5fea93f0a6770fac1fa9
vladskakun <52419346+vladskakun@users.noreply.github.com>
2021-09-11 20:30:36 +0300

    Change 'Click_checkbox method'

--- pages/issues.py
+++ pages/issues.py
@@ -7,50 +7,46 @@ class IssuesPage:
     Locators and methods for Issues page.
     """
 
-    VIEW_BUTTON_CSS = "tr:nth-child({})  a > button"
-    FROM_DATA_INPUT_CSS = "div:nth-child(1) > div > div > input"
-    TO_DATA_INPUT_CSS = "div:nth-child(2) > div > div > input"
-    CHECKBOX_OPEN_CSS = "input[name='status[0]']"
-    CHECKBOX_IN_PROGRESS_CSS = "input[name='status[1]']"
-    CHECKBOX_RESOLVE_CSS = "input[name='status[2]']"
-    RESET_BUTTON_XPATH = "//span[contains(text(),'Reset')]"
-    SEARCH_BUTTON_XPATH = "//span[contains(text(),'Search')]"
+    VIEW_BTN_CSS = "tr:nth-child({})  a > button"
+    FROM_DATA_INP_CSS = "div:nth-child(1) > div > div > input"
+    TO_DATA_INP_CSS = "div:nth-child(2) > div > div > input"
+    CHECK_CSS = "input[name='status[{}]']"
+    RESET_BTN_XPATH = "//span[contains(text(),'Reset')]"
+    SEARCH_BTN_XPATH = "//span[contains(text(),'Search')]"
 
     def __init__(self):
         self.driver = webdriver.Chrome()
 
-    """
-    Method for click on a view button.
-    Need write factical number + 1.
-    """
     def click_view(self, number):
-        element = self.driver.find_element(By.CSS_SELECTOR, self.VIEW_BUTTON_CSS.format(number), number)
+        """
+            Method for click on a needed view button.
+        """
+        element = self.driver.find_element(By.CSS_SELECTOR, self.VIEW_BTN_CSS.format(number + 1), number)
         element.click()
 
-    #def from_data_input(self, data):
-        #element = self.driver.find_element(By.CSS_SELECTOR, self.FROM_DATA_INPUT_CSS)
-        #element
+    """
+        There should be a code for entering the date in the datepicker but I cannot fill it with data.
+        Please, help me...
+    """
 
-    #def to_data_input(self, data):
-        #element = self.driver.find_element(By.CSS_SELECTOR, self.TO_DATA_INPUT_CSS)
-        #element
-
-    def click_checkbox_open(self, number):
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CHECKBOX_OPEN_CSS)
-        element.click()
-
-    def click_checkbox_in_progress(self, number):
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CHECKBOX_IN_PROGRESS_CSS)
-        element.click()
-
-    def click_checkbox_resolve(self, number):
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CHECKBOX_RESOLVE_CSS)
-        element.click()
+    def click_checkbox(self, key):
+        if key.lower() == "open":
+            element = self.driver.find_element(By.CSS_SELECTOR, self.CHECK_CSS.format('0'))
+            element.click()
+        elif key.lower() == "in progress":
+            element = self.driver.find_element(By.CSS_SELECTOR, self.CHECK_CSS.format('1'))
+            element.click()
+        elif key.lower() == "resolve":
+            element = self.driver.find_element(By.CSS_SELECTOR, self.CHECK_CSS.format('2'))
+            element.click()
+        else:
+            return "checkbox not find"
 
     def click_reset_button(self):
-        element = self.driver.find_element(By.XPATH, self.RESET_BUTTON_XPATH)
+        element = self.driver.find_element(By.XPATH, self.RESET_BTN_XPATH)
         element.click()
 
     def click_search_button(self):
-        element = self.driver.find_element(By.XPATH, self.SEARCH_BUTTON_XPATH)
+        element = self.driver.find_element(By.XPATH, self.SEARCH_BTN_XPATH)
         element.click()
+
