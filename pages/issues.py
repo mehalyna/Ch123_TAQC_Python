615560455887ad0f22ee5f7c5ac6bc98e1b938c8
vladskakun <52419346+vladskakun@users.noreply.github.com>
2021-09-13 20:07:47 +0300

    Reformate code on `Issue` page

--- pages/issues.py
+++ pages/issues.py
@@ -3,59 +3,39 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class IssueStatuses(Enum):
-    """ Enum of types for Issue Statuses. """
-    OPEN = '0'
-    IN_PROGRESS = '1'
-    RESOLVE = '2'
-
-
 class IssuesPage:
     """ Locators and methods for Issues page. """
 
     VIEW_BTN_CSS = "tr:nth-child({})  a > button"
-    FROM_DATA_INP_CSS = "div:nth-child(1) > div > div > input"
-    TO_DATA_INP_CSS = "div:nth-child(2) > div > div > input"
-    CHECK_CSS = "input[name='status[{}]']"
-    RESET_BTN_XPATH = "//span[contains(text(),'Reset')]"
-    SEARCH_BTN_XPATH = "//span[contains(text(),'Search')]"
+    CHECK_CSS = ".checkbox > label"
+    BTN_CSS = "form > div.d-flex > button"
 
     def __init__(self):
         self.driver = webdriver.Chrome()
 
     def click_view(self, view_number):
         """
-         Method for click on a needed view button.
-
-                Paremeters:
-                    view_number (int): A decimal integer
-
+            Method for click on a needed view button.
         """
         element = self.driver.find_element(By.CSS_SELECTOR, self.VIEW_BTN_CSS.format(view_number + 1), view_number)
         element.click()
 
-    """
-        There should be a code for entering the date in the datepicker but I cannot fill it with data.
-        Please, help me...
-    """
-
     def click_issue_status_filter(self, issue_status):
         """
-            Method for click on a needed issue status checkbox.
+            Method for click on issue status checkbox depending on text value.
         """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CHECK_CSS.format(issue_status.value))
-        element.click()
+        elements = self.driver.find_elements(By.CSS_SELECTOR, self.CHECK_CSS)
+        for element in elements:
+            if issue_status == element.text:
+                element.click()
+                return
 
-    def click_reset_button(self):
+    def click_status_button(self, name_button):
         """
-            Method for click on a 'Reset' button.
+            Method for click on button depending on text value.
         """
-        element = self.driver.find_element(By.XPATH, self.RESET_BTN_XPATH)
-        element.click()
-
-    def click_search_button(self):
-        """
-            Method for click on a 'Search' button.
-        """
-        element = self.driver.find_element(By.XPATH, self.SEARCH_BTN_XPATH)
-        element.click()
+        elements = self.driver.find_element(By.CSS_SELECTOR, self.BTN_CSS)
+        for element in elements:
+            if name_button == element.text:
+                element.click()
+                return
