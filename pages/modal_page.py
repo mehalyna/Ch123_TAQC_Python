3b88ce2834d9f8276b491a3c5b134edff412aa61
vladskakun <52419346+vladskakun@users.noreply.github.com>
2021-09-13 20:12:02 +0300

    Reformate code on 'landing','modal_page', 'nav_menu' page.

--- pages/modal_page.py
+++ pages/modal_page.py
@@ -4,59 +4,33 @@ from selenium.webdriver.common.by import By
 
 class ModalPage:
     """
-    Locators and methods for Modal page.
+        Locators and methods for Modal page.
     """
     MODAL_DIALOG_XPATH = "//div[@class='MuiDialog-root'][2]"
-    FORM_LOGIN_PAGE_XPATH = f"{MODAL_DIALOG_XPATH}//span[text() = 'Login']"
-    FORM_EMAIL_INPUT_XPATH = f"{MODAL_DIALOG_XPATH}//input[@name='email']"
-    FORM_PASSWORD_INPUT_XPATH = f"{MODAL_DIALOG_XPATH}//input[@name='password']"
-    FORM_SIGN_IN_BUTTON_XPATH = f"{MODAL_DIALOG_XPATH}//button[@value='Login']"
-    FORM_FORGOT_BUTTON_XPATH = f"{MODAL_DIALOG_XPATH}//span[contains(text(),'Forgot password')]"
-    FORM_CLEAR_BUTTON_XPATH = f"{MODAL_DIALOG_XPATH}//span[text() = 'CLEAR']"
-    FORM_CANCEL_BUTTON_XPATH = f"{MODAL_DIALOG_XPATH}//span[text() = 'Cancel']"
-    FORM_REGISTER_PAGE_XPATH = f"{MODAL_DIALOG_XPATH}//span[text() = 'Register']"
-    FORM_REGISTER_PASSWORD_REPEAT_INPUT_XPATH = f"{MODAL_DIALOG_XPATH}//input[@name='RepeatPassword']"
-    FORM_REGISTER_SIGN_UP_BUTTON_XPATH = f"{MODAL_DIALOG_XPATH}//span[text() = 'Sign Up']"
+    FORM_PAGE_XPATH = f"{MODAL_DIALOG_XPATH}//span[text() = '{{}}']"
+    FORM_INP_XPATH = f"{MODAL_DIALOG_XPATH}//input[@name='{{}}']"
+    FORM_BTN_XPATH = f"{MODAL_DIALOG_XPATH}//span[text() = '{{}}']"
 
     def __init__(self):
         self.driver = webdriver.Chrome()
 
-    def click_log_in_page(self):
-        element = self.driver.find_element(By.XPATH, self.FORM_LOGIN_PAGE_XPATH)
+    def click_page(self, page_title):
+        """
+            Method for click on page depending on text value.
+        """
+        element = self.driver.find_element(By.XPATH, self.FORM_PAGE_XPATH.format(page_title), page_title)
         element.click()
 
-    def send_email_input(self, string):
-        element = self.driver.find_element(By.XPATH, self.FORM_EMAIL_INPUT_XPATH)
-        element.send_keys(string)
-
-    def send_password_input(self, string):
-        element = self.driver.find_element(By.XPATH, self.FORM_PASSWORD_INPUT_XPATH)
-        element.send_keys(string)
-
-    def click_sign_in(self):
-        element = self.driver.find_element(By.XPATH, self.FORM_SIGN_IN_BUTTON_XPATH)
+    def send_data_input(self, name_input):
+        """
+            Method for send data to input depending on text value.
+        """
+        element = self.driver.find_element(By.XPATH, self.FORM_INP_XPATH.format(name_input), name_input)
         element.click()
 
-    def click_forgot(self):
-        element = self.driver.find_element(By.XPATH, self.FORM_FORGOT_BUTTON_XPATH)
-        element.click()
-
-    def click_clear(self):
-        element = self.driver.find_element(By.XPATH, self.FORM_CLEAR_BUTTON_XPATH)
-        element.click()
-
-    def click_cancel(self):
-        element = self.driver.find_element(By.XPATH, self.FORM_CANCEL_BUTTON_XPATH)
-        element.click()
-
-    def click_regiser_page(self):
-        element = self.driver.find_element(By.XPATH, self.FORM_REGISTER_PAGE_XPATH)
-        element.click()
-
-    def send_password_repeat_input(self, string):
-        element = self.driver.find_element(By.XPATH, self.FORM_REGISTER_PASSWORD_REPEAT_INPUT_XPATH)
-        element.send_keys(string)
-
-    def click_sign_up(self):
-        element = self.driver.find_element(By.XPATH, self.FORM_REGISTER_SIGN_UP_BUTTON_XPATH)
+    def click_button(self, name_button):
+        """
+            Method for click on button depending on text value.
+        """
+        element = self.driver.find_element(By.XPATH, self.FORM_BTN_XPATH.format(name_button), name_button)
         element.click()
--- pages/nav_menu.py
+++ pages/nav_menu.py
@@ -4,35 +4,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from telnetlib import EC
 
+
 class NavigationPage:
     """
         Locators and methods for navigation menu.
     """
-    NAV_HOME_XPATH = "//span[text() = 'Home']"
-    NAV_COMUNA_XPATH = "//span[text() = 'Comuna']"
-    NAV_ADMIN_XPATH = "//span[text() = 'Admin']"
-    NAV_ISSUES_XPATH = "//span[text() = 'Issues']"
-    NAV_EDIT_PROFILE_CSS = ".svg-inline--fa.fa-cog.fa-w-16"
-    NAV_LOGOUT_XPATH = "//a[@href='/home/events?page=1&radius=8']"
+    NAV_PAGE_TITLE_CSS = "span.nav-item-text"
+    NAV_EDIT_PROFILE_CSS = "a:nth-child(1) > button"
+    NAV_LOGOUT_CSS = "a:nth-child(2) > button"
 
     def __init__(self):
         self.driver = webdriver.Chrome()
 
-    def click_navigation_home(self):
-        element = self.driver.find_element(By.XPATH, self.NAV_HOME_XPATH)
-        element.click()
-
-    def click_navigation_comuna(self):
-        element = self.driver.find_element(By.XPATH, self.NAV_COMUNA_XPATH)
-        element.click()
-
-    def click_navigation_admin(self):
-        element = self.driver.find_element(By.XPATH, self.NAV_ADMIN_XPATH)
-        element.click()
-
-    def click_navigation_issues(self):
-        element = self.driver.find_element(By.XPATH, self.NAV_ISSUES_XPATH)
-        element.click()
+    def click_navigation_page(self, page_title):
+        """
+            Method for click on navigation menu depending on text value.
+        """
+        elements = self.driver.find_element(By.CSS_SELECTOR, self.NAV_PAGE_TITLE_CSS)
+        for element in elements:
+            if page_title == element.text:
+                element.click()
+                return
 
     def click_navigation_edit_profile(self):
         element = self.driver.find_element(By.CSS_SELECTOR, self.NAV_EDIT_PROFILE_CSS)
@@ -41,7 +33,7 @@ class NavigationPage:
     def click_log_out(self, wait_time=10):
         wait = WebDriverWait(self.driver, wait_time)
         element = wait.until(
-            EC.element_to_be_clickable(By.XPATH, self.NAV_LOGOUT_XPATH)
+            EC.element_to_be_clickable(By.XPATH, self.NAV_LOGOUT_CSS)
         )
         actions = ActionChains(self.driver)
         actions.move_to_element(element).perform()
