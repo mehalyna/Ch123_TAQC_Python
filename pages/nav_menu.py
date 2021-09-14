952f82c18e6e22e16efd7545e179a711006d31a3
vladskakun <52419346+vladskakun@users.noreply.github.com>
2021-09-14 10:39:51 +0300

    add docstirng to go_to_page

--- pages/nav_menu.py
+++ pages/nav_menu.py
@@ -19,6 +19,19 @@ class NavigationPage:
     def go_to_page(self, page_title):
         """
             Method for click on page depending on page_title value.
+            page_title values for admin:
+                'Home' - Home page
+                'Comuna' - Comuna page
+                'Admin' - Admin page
+                'Issues' - Issues page
+            page_title for user:
+                'Home' - Home page
+                'Profile' - Profile page
+                'Draft' - Drafts page
+                'Search Users' - Search/users page
+                'Recurrent Events' - eventSchedules page
+                'Contact us' - contactAdmin page
+                'Comuna' Comuna page
         """
         elements = self.driver.find_elements(By.CSS_SELECTOR, self.NAV_PAGE_TITLE_CSS)
         for element in elements:
