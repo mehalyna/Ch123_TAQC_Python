142aef56c968318dfe4b4d0b2192c776b48e7ceb
vladskakun <52419346+vladskakun@users.noreply.github.com>
2021-09-15 21:35:28 +0300

    add dockstring

--- pages/basePage.py
+++ pages/basePage.py
@@ -6,23 +6,38 @@ from telnetlib import EC
 
 class BasePage:
     def __init__(self):
+        """
+            Method for class fields declaration.
+        """
         self.driver = webdriver.Chrome()
         self.base_url = "https://eventsexpress-test.azurewebsites.net/"
 
     def find_element_by_css(self, locator, time=10):
+        """
+            Method for search element for css selector with wait
+        """
         WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
                                                message=f"Can't find element by locator {locator}")
         return self.driver.find_element(By.CSS_SELECTOR, locator)
 
     def find_element_by_xpath(self, locator, time=10):
+        """
+            Method for search element for xpath selector with wait
+        """
         WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
                                                message=f"Can't find element by locator {locator}")
         return self.driver.find_element(By.XPATH, locator)
 
     def find_elements(self, locator, time=10):
+        """
+            Method for search elements for css selector with wait
+        """
         WebDriverWait(self.driver, time).until(EC.presence_of_all_elements_located(locator),
                                                message=f"Can't find elements by locator {locator}")
         return self.driver.find_elements(By.CSS_SELECTOR, locator)
 
     def go_to_site(self):
+        """
+            Method for go to the base_url
+        """
         return self.driver.get(self.base_url)
