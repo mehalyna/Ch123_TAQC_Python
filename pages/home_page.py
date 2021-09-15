d09aa5d6d6d17f39c00d9625f68a9b90434b9838
VlaDFykeR <Fykerlikeagod1@gmail.com>
2021-09-15 22:33:42 +0300

    edited by convention and added docstrings

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,66 +1,115 @@
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 class HomePage:
     """
-    Locators and methods for Home page.
+        Locators for Hope page.
     """
-    CARD_LINK_TO_EVENTS_XPATH ="//div[@class='MuiCardMedia-root']/a"
-    KEYWORD_INPUT_CSS = "form > div:nth-child(1) > div > div > input"
-    MORE_FILTERS_BUTTON_CSS = "div:nth-child(2) > button > span.MuiButton-label"
-    DATA_FROM_INPUT_CSS = "form > div:nth-child(2) > div > div > input"
-    DATA_TO_INPUT_CSS = "form > div:nth-child(3) > div > div > input"
-    HASHTAGS_INPUT_CSS = "div:nth-child(4) > div > div > div > input"
-    FILTER_BY_LOCATION_BUTTON_CSS = "div:nth-child(6) > div > button > span.MuiButton-label"
-    LESS_BUTTON_CSS = "div:nth-child(8) > button > span.MuiButton-label"
-    RESET_BUTTON_CSS = "div.d-flex > button:nth-child(1) > span.MuiButton-label"
-    FAVORITE_BUTTON_CSS = "div.d-flex > button:nth-child(2) > span.MuiButton-label"
-    SEARCH_BUTTON_CSS = "div.d-flex > button:nth-child(3) > span.MuiButton-label"
+    HOME_PAGE_CARD_TO_EVENTS_LINK_CSS = "div:nth-child({}) > div > div.MuiCardMedia-root"
+    HOME_PAGE_NUMBER_OF_PAGE_BTN_CSS = "ul > div > div > button:nth-child({})"
+    HOME_PAGE_KEYWORD_INP_CSS = "form > div:nth-child(1) > div > div > input"
+    HOME_PAGE_MORE_FILTERS_BTN_CSS = "div:nth-child(2) > button > span.MuiButton-label"
+    HOME_PAGE_RESET_BTN_CSS = "div.d-flex > button:nth-child(1) > span.MuiButton-label"
+    HOME_PAGE_FAVORITE_BTN_CSS = "div.d-flex > button:nth-child(2) > span.MuiButton-label"
+    HOME_PAGE_SEARCH_BTN_CSS = "div.d-flex > button:nth-child(3) > span.MuiButton-label"
+    """
+        Locators for More filters menu.
+    """
+    MORE_FILTERS_MENU_DATE_FROM_INP_CSS = "form > div:nth-child(2) > div > div > input"
+    MORE_FILTERS_MENU_DATE_TO_INP_CSS = "form > div:nth-child(3) > div > div > input"
+    MORE_FILTERS_MENU_HASHTAGS_INP_CSS = "div:nth-child(4) > div > div > div > input"
+    MORE_FILTERS_MENU_CHECK_CSS = "div.checkbox > label"
+    MORE_FILTERS_MENU_FILTER_BY_LOCATION_BTN_CSS = "div:nth-child(6) > div > button > span.MuiButton-label"
+    MORE_FILTERS_MENU_LESS_BTN_CSS = "div:nth-child(8) > button > span.MuiButton-label"
+    """ 
+        Methods for Home page and More filters menu.
+    """
+    def __init__(self):
+        self.driver = webdriver.Chrome()
 
-    def __init__(self, driver):
-        self.driver = driver
+    def click_to_event_link(self, index):
+        """
+            Method for click card to event.
+            Index must contains integer value.
+        """
+        self.driver.find_element(By.CSS_SELECTOR, self.HOME_PAGE_CARD_TO_EVENTS_LINK_CSS.format(index)).click()
 
-    def click_link_to_event(self, index):
-        self.driver.find_elements(By.XPATH, self.CARD_LINK_TO_EVENTS_XPATH)[index].click()
-
-    def get_pagination(self, number_of_page):
-        return f"ul > div > div > button:nth-child({number_of_page})"
-
-    def click_page_btn(self, index):
-        self.driver.find_element(By.CSS_SELECTOR, self.get_pagination(index)).click()
+    def click_number_of_page_btn(self, index):
+        """
+            Method for click number of page.
+            Index must contains integer value.
+        """
+        self.driver.find_elements(By.CSS_SELECTOR, self.HOME_PAGE_NUMBER_OF_PAGE_BTN_CSS.format(index)).click()
 
     def send_keyword_input(self, string):
-        self.driver.find_element(By.CSS_SELECTOR, self.KEYWORD_INPUT_CSS).send_keys(string)
+        """
+            Method for input text in 'keyword' field.
+        """
+        self.driver.find_element(By.CSS_SELECTOR, self.HOME_PAGE_KEYWORD_INP_CSS).send_keys(string)
 
     def click_more_filters_btn(self):
-        self.driver.find_element(By.CSS_SELECTOR, self.MORE_FILTERS_BUTTON_CSS).click()
+        """
+            Method for open 'more filters menu'.
+        """
+        self.driver.find_element(By.CSS_SELECTOR, self.HOME_PAGE_MORE_FILTERS_BTN_CSS).click()
 
-    def send_data_from_input(self, data):
-        self.driver.find_element(By.CSS_SELECTOR, self.DATA_FROM_INPUT_CSS).send_keys(data)
+    def send_date_from_input(self, date):
+        """
+            Method for input date in 'date from' field.
+            Date must contains format like this 'dd-mm-yyyy'.
+        """
+        self.driver.find_element(By.CSS_SELECTOR, self.MORE_FILTERS_MENU_DATE_FROM_INP_CSS).send_keys(date)
 
-    def send_data_to_input(self, data):
-        self.driver.find_element(By.CSS_SELECTOR, self.DATA_TO_INPUT_CSS).send_keys(data)
+    def send_date_to_input(self, date):
+        """
+            Method for input date in 'date to' field.
+            Date must contains format like this 'dd-mm-yyyy'.
+        """
+        self.driver.find_element(By.CSS_SELECTOR, self.MORE_FILTERS_MENU_DATE_TO_INP_CSS).send_keys(date)
 
     def send_hashtags_input(self, string):
-        self.driver.find_element(By.CSS_SELECTOR, self.HASHTAGS_INPUT_CSS).send_keys(string)
+        """
+            Method for input text in 'hashtags' field.
+        """
+        self.driver.find_element(By.CSS_SELECTOR, self.MORE_FILTERS_MENU_HASHTAGS_INP_CSS).send_keys(string)
 
-    def get_checkboxes(self, number_of_checkbox):
-        return f"div:nth-child({number_of_checkbox}) > label > input[type=checkbox]"
-
-    def click_checkbox(self, active_blocked_canceled):
-        self.driver.find_element(By.CSS_SELECTOR, self.get_checkboxes(active_blocked_canceled)).click()
+    def click_active_blocked_canceled_checkbox(self, active_blocked_canceled):
+        """
+            Method for click checkboxes in 'More filters' menu depending on text value.
+        """
+        elements = self.driver.find_elements(By.CSS_SELECTOR, self.MORE_FILTERS_MENU_CHECK_CSS)
+        for element in elements:
+            if active_blocked_canceled in element.text:
+                element.click()
+                return
 
     def click_filter_by_location_btn(self):
-        self.driver.find_element(By.CSS_SELECTOR, self.FILTER_BY_LOCATION_BUTTON_CSS).click()
+        """
+            Method for click 'filter by location' button.
+        """
+        self.driver.find_element(By.CSS_SELECTOR, self.MORE_FILTERS_MENU_FILTER_BY_LOCATION_BTN_CSS).click()
 
     def click_less_btn(self):
-        self.driver.find_element(By.CSS_SELECTOR, self.LESS_BUTTON_CSS).click()
+        """
+            Method for click 'less' button.
+        """
+        self.driver.find_element(By.CSS_SELECTOR, self.MORE_FILTERS_MENU_LESS_BTN_CSS).click()
 
     def click_reset_btn(self):
-        self.driver.find_element(By.CSS_SELECTOR, self.RESET_BUTTON_CSS).click()
+        """
+            Method for click 'reset' button.
+        """
+        self.driver.find_element(By.CSS_SELECTOR, self.HOME_PAGE_RESET_BTN_CSS).click()
 
     def click_favorite_btn(self):
-        self.driver.find_element(By.CSS_SELECTOR, self.FAVORITE_BUTTON_CSS).click()
+        """
+            Method for click 'favorite' button.
+        """
+        self.driver.find_element(By.CSS_SELECTOR, self.HOME_PAGE_FAVORITE_BTN_CSS).click()
 
     def click_search_btn(self):
-        self.driver.find_element(By.CSS_SELECTOR, self.SEARCH_BUTTON_CSS).click()
+        """
+            Method for click 'search' button.
+        """
+        self.driver.find_element(By.CSS_SELECTOR, self.HOME_PAGE_SEARCH_BTN_CSS).click()
