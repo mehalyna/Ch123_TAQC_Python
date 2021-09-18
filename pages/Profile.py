4e6c5137ac1f117a1c6cc3d6098ff5cf7bbdb9de
NastasiaLu <nasta.lutchenko@gmail.com>
2021-09-19 02:17:41 +0300

    Task6_Profile_ added docstring added comments Update CSS selectors

--- pages/Profile.py
+++ pages/Profile.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from pages.elements.datepickers import DatePicker
+from pages.elements.buttonElement import ButtonElement
 
 
 class ProfilePage:
@@ -9,9 +10,7 @@ class ProfilePage:
        Locators and methods for Profile Page.
     """
     EDIT_PROFILE_BTN_CSS = "a:nth-child(1) > button"
-    """ 
-       Expansion panel Locators
-    """
+    #Expansion panel Locators
     EXPANSION_PANEL_CSS = '.MuiExpansionPanel-root:nth-child({})'
     EXPANSION_PANEL_AVATAR_CHANGE_BTN_CSS = ".MuiExpansionPanel-root:nth-child(1)"
     EXPANSION_PANEL_USERNAME_BTN_CSS = ".MuiExpansionPanel-root:nth-child(2)"
@@ -21,50 +20,34 @@ class ProfilePage:
     EXPANSION_PANEL_MANAGE_NOTIFICATION_BTN_CSS = ".MuiExpansionPanel-root:nth-child(6)"
     EXPANSION_PANEL_LINKED_ACCOUNTS_BTN_CSS = ".MuiExpansionPanel-root:nth-child(7)"
     EXPANSION_PANEL_CHANGE_PASSWORD_BTN_CSS = ".MuiExpansionPanel-root:nth-child(8)"
-    """
-       Locators for action inside expansion panels manage notification 
-    """
+    #Locators for action inside expansion panels manage notification
     MANAGE_NOTIFICATION_SAVE_BTN_CSS = ".MuiExpansionPanel-root:nth-child(6)  button[type = 'submit']"
-    """
-       Locators for action inside expansion panels Username
-    """
+    #Locators for action inside expansion panels Username
     USERNAME_INP_CSS = "input[name^='userName']"
     USERNAME_CLEAR_BTN_CSS = ".MuiExpansionPanel-root:nth-child(2)  button[type= 'button'] "
     USERNAME_SUBMIT_BTN_CSS = ".MuiExpansionPanel-root:nth-child(2)  button[type= 'submit'] "
-    """
-       Locators for action inside expansion panels Change Avatar
-    """
+    #Locators for action inside expansion panels Change Avatar
     CHANGE_AVATAR_CLEAR_BTN_CSS = ".MuiExpansionPanel-root:nth-child(1)  button[type= 'button']"
     CHANGE_AVATAR_SUBMIT_BTN_CSS = ".MuiExpansionPanel-root:nth-child(1)  button[type= 'submit']"
     CHANGE_AVATAR_CROP_BTN_CSS = ".ImageResizer  button[type= 'button'] "
     CHANGE_AVATAR_UPLOAD_NEW_PICTURE = "#panel1bh-content > div > p > form > div:nth-child(1) > div > div > div"
-    """
-       Locators for action inside expansion panels Gender
-    """
-    GENDER_DROPDOWN_CSS = "#panel2bh-content > div > p > form > div:nth-child(1) > div"
+    #Locators for action inside expansion panels Gender
+    GENDER_DROPDOWN_CSS = ".MuiExpansionPanel-root:nth-child(3)  form > div:nth-child(1) > div"
     GENDER_OPT_CSS = "div > select"
     GENDER_SUBMIT_BTN_CSS = ".MuiExpansionPanel-root:nth-child(3)  button[type= 'submit']"
-    """
-       Locators for action inside expansion panels Date of Birth
-    """
+    #Locators for action inside expansion panels Date of Birth
     DATE_OF_BIRTH_DATAPICKER_CSS = "input[type ='date']"
     DATE_OF_BIRTH_CLEAR_BTN_CSS = ".MuiExpansionPanel-root:nth-child(4)  button[type= 'button']"
     DATE_OF_BIRTH_SUBMIT_BTN_CSS = ".MuiExpansionPanel-root:nth-child(4)  button[type= 'submit']"
-    """ 
-       Locators for action inside expansion panels Favorite Categories
-    """
-    FAVORITE_CATEGORIES_SELECT_CATEGORIES_FORM_CSS = "#panel4bh-content > div > p > div > form "
+    #Locators for action inside expansion panels Favorite Categories
+    FAVORITE_CATEGORIES_SELECT_CATEGORIES_FORM_CSS = ".MuiExpansionPanel-root:nth-child(5)  form"
     FAVORITE_CATEGORIES_CATEGORIES_LIST = "#rw_1_listbox li"
     FAVORITE_CATEGORIES_SAVE_BTN_CSS = ".MuiExpansionPanel-root:nth-child(5)  button[type= 'submit']"
-    """
-       Locators for action inside expansion panels Linked Accounts
-    """
+    #Locators for action inside expansion panels Linked Accounts
     LINKED_ACCOUNTS_GOOGLE_BTN_CSS = ".btnGoogle > i"
     LINKED_ACCOUNTS_MAIL_BTN_CSS = ".btnGoogle > svg"
     LINKED_ACCOUNTS_FACEBOOK_BTN_CSS = ".btnFacebook"
-    """ 
-       Locators for action inside expansion panels Change Password 
-    """
+    #Locators for action inside expansion panels Change Password
     CHANGE_PASSWORD_CURRENT_PASSWORD_INP_CSS = "input[name^='oldPassword']"
     CHANGE_PASSWORD_NEW_PASSWORD_INP_CSS = "input[name^='newPassword']"
     CHANGE_PASSWORD_REPEAT_NEW_PASSWORD_INP_CSS = "input[name^='repeatPassword']"
@@ -74,7 +57,13 @@ class ProfilePage:
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.expansion_panel_date_of_birth_dtp = DatePicker(self.DATE_OF_BIRTH_DATAPICKER_CSS)
-
+        self.username_clear_btn = ButtonElement(self.USERNAME_CLEAR_BTN_CSS)
+        self.username_submit_btn = ButtonElement(self.USERNAME_SUBMIT_BTN_CSS)
+        self.favorite_categories_save_btn = ButtonElement(self.FAVORITE_CATEGORIES_SAVE_BTN_CSS)
+        self.password_submit_btn = ButtonElement(self.CHANGE_PASSWORD_SUBMIT_BTN_CSS)
+        self.password_clear_btn = ButtonElement(self.CHANGE_PASSWORD_CLEAR_BTN_CSS)
+        self.date_of_birth_clear_btn = ButtonElement(self.DATE_OF_BIRTH_CLEAR_BTN_CSS)
+        self.date_of_birth_submit_btn = ButtonElement(self.DATE_OF_BIRTH_SUBMIT_BTN_CSS)
 
     def click_edit_your_profile_btn(self):
         """
@@ -85,8 +74,16 @@ class ProfilePage:
 
     def click_expansion_panel(self, expansion_panel_number):
         """
-            Method for click on a needed expansion_panel button
-
+            Method for click on a needed expansion panel button
+            int expansion_panel_number:
+            1 = Change Avatar
+            2 = Username
+            3 = Gender
+            4 = Date of Birth
+            5 = Favorite Categories
+            6 = Manage Notification
+            7 = Linked Accounts
+            8 = Change password
         """
         element = self.driver.find_element(By.CSS_SELECTOR, self.EXPANSION_PANEL_CSS.format(expansion_panel_number))
         element.click()
@@ -104,6 +101,7 @@ class ProfilePage:
         element = self.driver.find_element(By.CSS_SELECTOR, self.CHANGE_AVATAR_SUBMIT_BTN_CSS)
         element.click()
 
+
     def input_new_username(self, new_name):
         """
             Method to input new username
@@ -114,21 +112,6 @@ class ProfilePage:
         self.driver.implicitly_wait(10)
         element1 = self.driver.find_element(By.CSS_SELECTOR, self.USERNAME_INP_CSS)
         element1.send_keys(new_name)
-        self.driver.implicitly_wait(10)
-
-    def click_username_clear_btn(self):
-        """
-            Method for clear field for input new username
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.USERNAME_CLEAR_BTN_CSS)
-        element.click()
-
-    def click_username_submit_btn(self):
-        """
-            Method for clear field for input new username
-        """
-        element2 = self.driver.find_element(By.CSS_SELECTOR, self.USERNAME_SUBMIT_BTN_CSS)
-        element2.click()
 
 
     def change_gender_option(self, option):
@@ -140,14 +123,15 @@ class ProfilePage:
         element.click()
         element = self.driver.find_element(By.CSS_SELECTOR, self.GENDER_DROPDOWN_CSS)
         element.click()
-        dropdown1 = Select(self.driver.find_element(By.CSS_SELECTOR, self.GENDER_OPT_CSS))
-        dropdown1.select_by_visible_text(option)
+        dropdown = Select(self.driver.find_element(By.CSS_SELECTOR, self.GENDER_OPT_CSS))
+        dropdown.select_by_visible_text(option)
         element = self.driver.find_element(By.CSS_SELECTOR, self.GENDER_SUBMIT_BTN_CSS)
         element.click()
 
     def choose_favorite_categories(self, categories):
         """
-           Method for choosing_favorite_categories
+           Method for choosing favorite categories
+           string categories : Fishing, Football, Gaming, Golf, Meeting, Mount, Sea, Sport, Summer
         """
         element = self.driver.find_element(By.CSS_SELECTOR, self.EXPANSION_PANEL_FAVORITE_CATEGORIES_BTN_CSS)
         element.click()
@@ -160,13 +144,6 @@ class ProfilePage:
             if el.text == categories:
                 self.driver.implicitly_wait(10)
                 el.click()
-
-    def click_favorite_categories_save_btn(self, categories):
-        """
-           Method for choosing_favorite_categories
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.FAVORITE_CATEGORIES_SAVE_BTN_CSS)
-        element.click()
 
     def change_password(self, current_password, new_password):
         """
@@ -185,21 +162,6 @@ class ProfilePage:
         element = self.driver.find_element(By.CSS_SELECTOR, self.CHANGE_PASSWORD_REPEAT_NEW_PASSWORD_INP_CSS)
         element.send_keys(new_password)
         self.driver.implicitly_wait(10)
-
-
-    def click_password_submit_btn(self):
-        """
-           Method for clear all change password fields
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CHANGE_PASSWORD_SUBMIT_BTN_CSS)
-        element.click()
-
-    def click_password_clear_btn(self):
-        """
-           Method for clear all change password fields
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CHANGE_PASSWORD_CLEAR_BTN_CSS)
-        element.click()
 
     def click_linked_account_google_btn(self):
         """
@@ -228,20 +190,6 @@ class ProfilePage:
         element = self.driver.find_element(By.CSS_SELECTOR, self.LINKED_ACCOUNTS_MAIL_BTN_CSS)
         element.click()
 
-
-    def click_date_of_birth_clear_btn(self):
-        """
-            Method to click clear button on expansion panel date of birth
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.DATE_OF_BIRTH_CLEAR_BTN_CSS)
-        element.click()
-
-    def click_date_of_birth_submit_btn(self):
-        """
-            Method to click clear button on expansion panel date of birth
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.DATE_OF_BIRTH_SUBMIT_BTN_CSS)
-        element.click()
 
 
 
