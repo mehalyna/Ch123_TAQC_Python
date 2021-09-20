ab8e6b98cb3a32684167a99da863d71d2e912cd0
Оля <dolitsoy.olha@chnu.edu.ua>
2021-09-20 14:40:46 +0300

    Fixed locators

--- pages/edit_categoty.py
+++ pages/edit_categoty.py
@@ -4,106 +4,85 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+
 class AdminAddCategoryPage:
     """
         Locators and methods for Admin Edit category page
     """
     index_of_row = 0
-    CTG_ADD_BTN_CSS = "button.ml-0"
-    CTG_ADD_INP_FIELD_CSS = "input.MuiInputBase-input"
-    CTG_ADD_SUBMIT_BTN_CSS = "tr:nth-child(1) button.text-success"
-    CTG_ADD_CANCEL_BTN_CSS = "tr:nth-child(1) button.text-danger"
-    TABLE_OF_CTG_CSS = "table"
-    CTG_EDIT_BTN_CSS = "tr:nth-child({index_of_row})  button.text-info"
-    CTG_EDIT_NAME_INP_CSS = "tr:nth-child({index_of_row}) > td:nth-child(1)  input"
-    CTG_SUBMIT_EDIT_BTN_CSS = "tr:nth-child({index_of_row}) > td:nth-child(4)  button.text-success"
-    CTG_CANCEL_EDIT_BTN_CSS = "tr:nth-child({index_of_row}) > td:nth-child(4)  button.text-danger"
-    CTG_DELETE_BTN_CSS = "tr:nth-child({index_of_row}) > td:nth-child(5)  button"
-    NUMBER_OF_USERS_VALUE_CSS = "tr:nth-child({index_of_row}) > td:nth-child(2)"
-    NUMBER_OF_EVENTS_VALUE_CSS = "tr:nth-child({index_of_row}) > td:nth-child(3)"
+    CTG_ADD_BTN_CSS = ".ml-0"
+    CTG_INP_FIELD_CSS = "#save-form"
+    CTG_SUBMIT_BTN_CSS = ".text-success"
+    CTG_CANCEL_BTN_CSS = ".text-danger"
+    CTG_EDIT_BTN_CSS = ".text-info"
+    CTG_DELETE_BTN = "td:nth-child(5) button.text-danger"
+    NUMBER_OF_USERS_VALUE_CSS = "td:nth-child(2)"
+    NUMBER_OF_EVENTS_VALUE_CSS = "td:nth-child(3)"
     COLUMN_OF_TABLE_CSS = "tr > td:nth-child(1):not(td.align-middle)"
+    CTG_ROW = "tr:nth-child({})"
 
     def __init__(self):
         self.driver = webdriver.Chrome()
-
 
     def find_category_by_name(self, category_name, wait_time=10):
         """
             Search for a category row by name
         :param category_name: name of category which we want to find
+        :return row index of category
         """
         elements = WebDriverWait(self.driver, wait_time).until(
             EC.visibility_of_all_elements_located((By.CSS_SELECTOR, self.COLUMN_OF_TABLE_CSS)))
         for idx, element in enumerate(elements):
             if element.text == category_name:
+                self.index_of_row = idx + 1
                 return idx + 1
+
+    def get_row(self):
+        return self.driver.find_element_by_css_selector(self.CTG_ROW.format(self.index_of_row))
 
     def click_add_ctg_btn(self):
         """
-               Method for creating new category with pressing button
+            Method for pressing add category button
         """
         element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_ADD_BTN_CSS)
         element.click()
 
-    def enter_ctg_name(self, data_for_input):
+    def click_submit_btn(self):
         """
-              Method for entering new category name.
-        :param data_for_input: Variable data_for_input should contain text which
-              we need to enter (name of new category)
+            Method for pressing submit button
         """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_ADD_INP_FIELD_CSS)
-        element.send_keys(data_for_input)
-
-    def click_cancel_ctg_creating_btn(self):
-        """
-             Method for canceling of creating new category by pressing CANCEL button
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_ADD_CANCEL_BTN_CSS)
+        element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_SUBMIT_BTN_CSS)
         element.click()
 
-    def click_submit_ctg_creating_btn(self):
+    def click_cancel_btn(self):
         """
-            Method for pressing submit button to save new category
+            Method for pressing cansel button in some row
         """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_ADD_SUBMIT_BTN_CSS)
-        element.click()
+        row = self.get_row()
+        row.find_element(By.CSS_SELECTOR, self.CTG_SUBMIT_BTN_CSS).click()
 
-    def edit_ctg_name(self):
+    def input_data(self, string):
+        """
+            Method for entering some string into input field
+        :param string: data, that we need to input
+        """
+        element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_INP_FIELD_CSS)
+        element.send_keys(string)
+
+    def click_edit_ctg_btn(self):
         """
             Method for pressing edit category button
         """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_EDIT_BTN_CSS.format(index_of_row = self.index_of_row))
-        element.click()
-
-    def inp_new_ctg_name(self, input_data):
-        """
-            Method for clearing input field and entering new name of category
-        :param input_data: new name of category
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_EDIT_NAME_INP_CSS.format(index_of_row = self.index_of_row))
-        element.clear()
-        element.send_keys(input_data)
-
-    def click_submit_ctg_edit_btn(self):
-        """
-           Method for saving change of category name
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_SUBMIT_EDIT_BTN_CSS.format(index_of_row = self.index_of_row))
-        element.click()
-
-    def click_cancel_ctg_edit_btn(self):
-        """
-            Method for canceling change of category name
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_CANCEL_EDIT_BTN_CSS.format(index_of_row=self.index_of_row))
-        element.click()
+        row = self.get_row()
+        row.find_element(By.CSS_SELECTOR, self.CTG_EDIT_BTN_CSS).click()
 
     def click_delete_ctg_btn(self):
         """
-            Method for deleting existing category
+            Method for pressing delete category button
         """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_DELETE_BTN_CSS.format(index_of_row=self.index_of_row))
-        element.click()
+        row = self.get_row()
+        row.find_element(By.CSS_SELECTOR, self.CTG_DELETE_BTN).click()
+
 
 
 
