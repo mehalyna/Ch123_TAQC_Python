49fab39deca962b220293d651cc9c0c269bcf880
Оля <dolitsoy.olha@chnu.edu.ua>
2021-09-22 02:15:08 +0300

    used BaseWrapper

---
+++ pages/elements/tableElement.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.common.baseWrapper import BaseWrapper
+
+
+class TableElement(BaseWrapper):
+
+    def __init__(self, table_selector, driver):
+        super.__init__(driver)
+        self.table_selector = table_selector
+
+    def find_category_by_name(self, search_name, wait_time = 10):
+        """
+            Method looks for text in a specific table and returns
+            the row number in which the text is located
+        :param search_name - text, that we need to find in table
+        """
+        elements = self.driver.find_elements(By.CSS_SELECTOR, self.table_selector)
+        WebDriverWait(self.driver, wait_time).until(
+            EC.visibility_of_all_elements_located((By.CSS_SELECTOR, self.table_selector)))
+        for idx, element in enumerate(elements):
+            if element.text == search_name:
+                return idx
+
+
+
+
+
