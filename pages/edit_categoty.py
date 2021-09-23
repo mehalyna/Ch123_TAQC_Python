f3fb6649b52d9d3420decef17ad01728bc651c17
Оля <dolitsoy.olha@chnu.edu.ua>
2021-09-23 13:00:01 +0300

    Edited table elements

--- pages/edit_categoty.py
+++ pages/edit_categoty.py
@@ -2,11 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pages.elements.ButtonElement import ButtonElement
+from pages.common.baseWrapper import BaseWrapper
+from pages.elements.InputElement import InputElement
+from pages.elements.tableElement import TableElement
 
-from pages.elements.buttonElement import ButtonElement
 
-
-class AdminAddCategoryPage:
+class AdminAddCategoryPage(BaseWrapper):
     """
         Locators and methods for Admin Edit category page
     """
@@ -15,63 +17,23 @@ class AdminAddCategoryPage:
     CTG_INP_FIELD_CSS = "#save-form"
     CTG_SUBMIT_BTN_CSS = ".text-success"
     CTG_CANCEL_BTN_CSS = ".text-danger"
-    CTG_EDIT_BTN_CSS = ".text-info"
-    CTG_DELETE_BTN = "td:nth-child(5) button.text-danger"
-    NUMBER_OF_USERS_VALUE_CSS = "td:nth-child(2)"
-    NUMBER_OF_EVENTS_VALUE_CSS = "td:nth-child(3)"
+    CTG_EDIT_BTN_CSS = "{} .text-info"
+    CTG_DELETE_BTN = "{} td:nth-child(5) button.text-danger"
+    NUMBER_OF_USERS_VALUE_CSS = "{} td:nth-child(2)"
+    NUMBER_OF_EVENTS_VALUE_CSS = "{} td:nth-child(3)"
     COLUMN_OF_TABLE_CSS = "tr > td:nth-child(1):not(td.align-middle)"
-    CTG_ROW = "tr:nth-child({})"
 
-    def __init__(self):
+    def __init__(self, driver):
+        super().__init__(driver)
         self.driver = webdriver.Chrome()
         self.add_ctg_btn = ButtonElement(self.CTG_ADD_BTN_CSS)
         self.submit_btn = ButtonElement(self.CTG_SUBMIT_BTN_CSS)
+        self.ctg_name_inp = InputElement(self.CTG_INP_FIELD_CSS)
+        self.row_index = TableElement(self.COLUMN_OF_TABLE_CSS, self.driver)
+        self.edit_ctg_btn = ButtonElement(self.CTG_EDIT_BTN_CSS)
+        self.cancel_ctg_btn = ButtonElement(self.CTG_CANCEL_BTN_CSS)
+        self.delete_ctg_btn = ButtonElement(self.CTG_DELETE_BTN)
 
-    def find_category_by_name(self, category_name, wait_time=10):
-        """
-            Search for a category row by name
-        :param category_name: name of category which we want to find
-        :return row index of category
-        """
-        elements = self.driver.find_elements(By.CSS_SELECTOR, self.COLUMN_OF_TABLE_CSS)
-        WebDriverWait(self.driver, wait_time).until(
-            EC.visibility_of_all_elements_located((By.CSS_SELECTOR, self.COLUMN_OF_TABLE_CSS)))
-        for idx, element in enumerate(elements):
-            if element.text == category_name:
-                self.index_of_row = idx + 1
-                return idx + 1
-
-    def get_row(self):
-        return self.driver.find_element_by_css_selector(self.CTG_ROW.format(self.index_of_row))
-
-    def click_cancel_btn(self):
-        """
-            Method for pressing cansel button in some row
-        """
-        row = self.get_row()
-        row.find_element(By.CSS_SELECTOR, self.CTG_SUBMIT_BTN_CSS).click()
-
-    def input_data(self, string):
-        """
-            Method for entering some string into input field
-        :param string: data, that we need to input
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.CTG_INP_FIELD_CSS)
-        element.send_keys(string)
-
-    def click_edit_ctg_btn(self):
-        """
-            Method for pressing edit category button
-        """
-        row = self.get_row()
-        row.find_element(By.CSS_SELECTOR, self.CTG_EDIT_BTN_CSS).click()
-
-    def click_delete_ctg_btn(self):
-        """
-            Method for pressing delete category button
-        """
-        row = self.get_row()
-        row.find_element(By.CSS_SELECTOR, self.CTG_DELETE_BTN).click()
 
 
 
--- pages/elements/tableElement.py
+++ pages/elements/tableElement.py
@@ -1,31 +1,34 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.common.baseWrapper import BaseWrapper
 
 
+
 class TableElement(BaseWrapper):
 
     def __init__(self, table_selector, driver):
-        super.__init__(driver)
+        super().__init__(driver)
+        self.row_idx = None
         self.table_selector = table_selector
 
-    def find_category_by_name(self, search_name, wait_time = 10):
+    def find_element_in_row(self, search_name, wait_time = 10):
         """
             Method looks for text in a specific table and returns
             the row number in which the text is located
         :param search_name - text, that we need to find in table
         """
-        elements = self.driver.find_elements(By.CSS_SELECTOR, self.table_selector)
+        elements = self.find_element_by_css(self.table_selector)
         WebDriverWait(self.driver, wait_time).until(
             EC.visibility_of_all_elements_located((By.CSS_SELECTOR, self.table_selector)))
         for idx, element in enumerate(elements):
             if element.text == search_name:
-                return idx
+                row_idx = "tr:nth-child({})".format(idx)
+                return row_idx
 
-    def get_row(self, row_index):
-        return self.driver.find_element_by_css_selector('tr:nth-child({})'.format(row_index))
+
+
+
 
 
 
