4255f22e5d83b01cda7cb0bd509f81bc1e33e520
YuraGora <83367811+YuraGora@users.noreply.github.com>
2021-09-27 04:29:22 +0300

    Use elements

--- pages/Chat.py
+++ pages/Chat.py
@@ -1,42 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from pages.elements.InputElement import InputElement
+from pages.elements.buttonElement import ButtonElement
+
 
 class ChatPage:
     """
         Locators and methods for Chat Page.
     """
-    CHAT_SMALL_AVATAR_BTN_CLASS_NAME = "SmallAvatar"
+    CHAT_SMALL_AVATAR_BTN_CSS = ".SmallAvatar"
     MESSAGE_TEXTAREA_CSS = "textarea"
     SEND_BTN_CSS = "button[type='submit']"
 
     def __init__(self):
         self.driver = webdriver.Chrome()
-
-    def click_chat_small_avatar(self):
-        """
-            Method for click small avatar on chat page.
-        """
-        element = self.driver.find_element(By.CLASS_NAME, self.CHAT_SMALL_AVATAR_BTN_CLASS_NAME)
-        element.click()
+        self.small_avatar_btn = ButtonElement(self.CHAT_SMALL_AVATAR_BTN_CSS)
+        self.send_message = InputElement(self.MESSAGE_TEXTAREA_CSS)
+        self.click_send_btn = ButtonElement(self.SEND_BTN_CSS)
 
     def click_textarea(self):
         """
             Method for click on textarea 'Type your message...'
         """
         element = self.driver.find_element(By.CSS_SELECTOR, self.MESSAGE_TEXTAREA_CSS)
-        element.click()
-
-    def send_textarea(self, message):
-        """
-            Method for type message in text area 'Type your message...'
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.MESSAGE_TEXTAREA_CSS)
-        element.send_keys(message)
-
-    def click_send_button(self):
-        """
-            Method for click 'Send' button.
-        """
-        element = self.driver.find_element(By.CSS_SELECTOR, self.SEND_BTN_CSS)
         element.click()
