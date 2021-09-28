425c61da99b2b9369623fc7a2cb62e1c6c92ce8a
vladskakun <52419346+vladskakun@users.noreply.github.com>
2021-09-28 17:05:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """
-    Test data for logging in us admin
+    Test data for login as admin.
 """
 ADMIN_EMAIL = "admin@gmail.com"
 ADMIN_PASS = "1qaz1qaz"
-LANDING_SIGN_IN_GUTTON = "Sign In"
+LANDING_SIGN_IN_BUTTON = "Sign In"
 BASE_URL = "https://eventsexpress-test.azurewebsites.net/"
